8054775438574de3af17847d059a34968c109591
amish274 <86523294+amish274@users.noreply.github.com>
2026-08-23 11:20:33 +0530

    git file change for DB

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+// config/database.php - Single Source of Truth for Database Configuration
+
+return [
+    'host'     => '127.0.0.1',
+    'port'     => '3306',
+    'database' => 'employee_monitor',
+    'username' => 'employee_monitor',
+    'password' => '4ckaCg6L3yeEgF9.',
+];
